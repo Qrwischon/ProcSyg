7819e46796df33cb37742e14f5550b3578d042e1
Qrwischon <129544071+Qrwischon@users.noreply.github.com>
2024-11-04 14:41:30 +0100

    Dodaj pliki projektów.

---
+++ ProcSyg.cpp
@@ -0,0 +1,321 @@
+﻿// ProcSyg.cpp : Definiuje punkt wejścia dla aplikacji.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ProcSyg.h"
+#include <windows.h>
+#include <commdlg.h>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+
+
+
+
+#define MAX_LOADSTRING 100
+#define WNDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+#define WAVE_FORM_START_X 50
+#define WAVE_FORM_START_Y 100
+
+
+
+
+// Zmienne globalne:
+HINSTANCE hInst;                                // bieżące wystąpienie
+WCHAR szTitle[MAX_LOADSTRING];                  // Tekst paska tytułu
+WCHAR szWindowClass[MAX_LOADSTRING];            // nazwa klasy okna głównego
+FILE* wavFile = nullptr;                        // global file pointer
+std::wstring selectedFilePath;                  // global variable to store the file path
+bool FileLoaded = false;                         // Flag to indicate if a file has been loaded
+HWND hButtonPlay = NULL;                         // Handle for Play button
+HWND hButtonStop = NULL;                         // Handle for Stop button
+HWND hButtonExit = NULL;                         // Handle for Exit button
+std::vector<short> audioData;
+#pragma pack(push, 1) // Ensure structures are packed
+
+struct WAVHeader {
+    char riff[4]; // RIFF Header
+    unsigned int fileSize; // File size
+    char wave[4]; // WAVE Header
+    char fmt[4]; // FMT Header
+    unsigned int fmtSize; // Format size
+    unsigned short audioFormat; // Audio format
+    unsigned short numChannels; // Number of channels
+    unsigned int sampleRate; // Sampling frequency
+    unsigned int byteRate; // Bytes per second
+    unsigned short blockAlign; // Block alignment
+    unsigned short bitsPerSample; // Bits per sample
+    char data[4]; // DATA Header
+    unsigned int dataSize; // Size of data
+};
+
+#pragma pack(pop) // Restore packing
+
+
+// Przekaż dalej deklaracje funkcji dołączone w tym module kodu:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: W tym miejscu umieść kod.
+
+    // Inicjuj ciągi globalne
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_PROCSYG, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Wykonaj inicjowanie aplikacji:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PROCSYG));
+
+    MSG msg;
+
+    // Główna pętla komunikatów:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+//
+//  FUNKCJA: MyRegisterClass()
+//
+//  PRZEZNACZENIE: Rejestruje klasę okna.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PROCSYG));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = CreateSolidBrush(RGB(50, 50, 50));;
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_PROCSYG);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNKCJA: InitInstance(HINSTANCE, int)
+//
+//   PRZEZNACZENIE: Zapisuje dojście wystąpienia i tworzy okno główne
+//
+//   KOMENTARZE:
+//
+//        W tej funkcji dojście wystąpienia jest zapisywane w zmiennej globalnej i
+//        jest tworzone i wyświetlane okno główne programu.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Przechowuj dojście wystąpienia w naszej zmiennej globalnej
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNKCJA: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PRZEZNACZENIE: Przetwarza komunikaty dla okna głównego.
+//
+//  WM_COMMAND  - przetwarzaj menu aplikacji
+//  WM_PAINT    - Maluj okno główne
+//  WM_DESTROY  - opublikuj komunikat o wyjściu i wróć
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Analizuj zaznaczenia menu:
+            switch (wmId)
+            {
+            case ID_PLIK_OPEN:
+            {
+                OPENFILENAME file;            // common dialog box structure
+                WCHAR szFile[MAX_PATH] = { 0 }; // buffer for file name
+
+                // Initialize OPENFILENAME structure
+                ZeroMemory(&file, sizeof(file));
+                file.lStructSize = sizeof(file);
+                file.hwndOwner = hWnd;
+                file.lpstrFile = szFile;
+                file.nMaxFile = sizeof(szFile);
+                file.lpstrFilter = L"WAV Files\0*.wav\0All Files\0*.*\0";
+                file.nFilterIndex = 1;
+                file.lpstrFileTitle = NULL;
+                file.nMaxFileTitle = 0;
+                file.lpstrInitialDir = NULL;
+                file.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+                // Display the Open dialog box
+                if (GetOpenFileName(&file) == TRUE) {
+                    selectedFilePath = file.lpstrFile; // Save file path globally
+                    FileLoaded = true;
+                    InvalidateRect(hWnd, NULL, TRUE); // Force repaint to update screen
+
+
+                    // Optionally, open the file
+                    FILE* wavFile;
+
+                    if (_wfopen_s(&wavFile, selectedFilePath.c_str(), L"rb") != 0) {
+                        MessageBox(hWnd, L"Failed to open file!", L"Error", MB_OK | MB_ICONERROR);
+                        return 0;
+                    }
+
+                    WAVHeader Header;
+                    fread(&Header, sizeof(Header), 1, wavFile);
+
+                    audioData.resize(Header.dataSize / sizeof(short));
+                    fread(audioData.data(), sizeof(short), audioData.size(), wavFile);
+                    fclose(wavFile);
+
+                }
+            }
+            break;
+
+            case ID_BUTTON_PLAY:
+                PlaySound(selectedFilePath.c_str(), NULL, SND_FILENAME | SND_ASYNC);
+                break;
+
+
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_CREATE:
+        hButtonPlay = CreateWindow(
+            L"BUTTON",
+            L"Play",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            100,
+            450,
+            150,
+            50,
+            hWnd,
+            (HMENU)ID_BUTTON_PLAY,
+            (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+            NULL);
+
+        break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+
+        if (!FileLoaded) {
+            SetTextColor(hdc, RGB(0, 255, 255));
+            SetBkMode(hdc, TRANSPARENT);
+            TextOut(hdc, 50, 50, L"Welcome to ProcSyg!", 20);
+            TextOut(hdc, 50, 80, L"Please open a file to begin.", 27);
+        }
+        else {
+            SetTextColor(hdc, RGB(0, 128, 0));
+            SetBkMode(hdc, TRANSPARENT);
+            TextOut(hdc, 50, 50, L"File loaded successfully!", 25);
+            TextOut(hdc, 50, 80, L"You can now process the file.", 28);
+
+            // Draw the waveform
+            int width = 1200; // Set the width for the drawing area
+            int height = 1200; // Set the height for the drawing area
+            int samplesPerPixel = audioData.size() / width; // Calculate samples to draw per pixel
+
+            MoveToEx(hdc, 0, 0, NULL); // Start in the middle of the height
+
+            for (int x = 0; x < width; ++x) {
+                // Calculate the index in audioData
+                int index = x * samplesPerPixel;
+                if (index < audioData.size()) {
+                    int y = (audioData[index] / 32768.0) * (height / 2); // Normalize and scale the value
+                    LineTo(hdc, x, (height / 2) - y); // Draw line to the calculated point
+                }
+            }
+        }
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
+
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Procedura obsługi komunikatów dla okna informacji o programie.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ resource.h
@@ -0,0 +1,50 @@
+﻿//{{NO_DEPENDENCIES}}
+// Plik dołączany wygenerowany przez środowisko Microsoft Visual C++.
+// Używany przez: ProcSyg.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PROCSYG_DIALOG              102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PROCSYG                     107
+#define IDI_SMALL                       108
+#define IDC_PROCSYG                     109
+#define IDR_MAINFRAME                   128
+#define ID_PLIK_OPEN                    32771
+#define ID_PLIK_SAVE                    32772
+#define ID_PLIK_SAVEAS                  32773
+#define ID_VOLUME_UP                    32774
+#define ID_VOLUME_DOWN                  32775
+#define ID_VOLUME_MUTE                  32776
+#define ID_UP_                          32777
+#define ID_UP_32778                     32778
+#define ID_UP_32779                     32779
+#define ID_DOWN_                        32780
+#define ID_DOWN_32781                   32781
+#define ID_DOWN_32782                   32782
+#define ID_EFFECTS_COMPRESSOR           32783
+#define ID_EFFECTS_DISTORTION           32784
+#define ID_EFFECTS_PHASER               32785
+#define ID_EFFECTS_FLANGER              32786
+#define ID_EFFECTS_CHORUS               32787
+#define ID_EFFECTS_VINTAGE              32788
+#define ID_EFFECTS_DELAY                32789
+#define ID_EFFECTS_LOOP                 32790
+#define ID_EFFECTS_REVERSE              32791
+#define ID_BUTTON_PLAY					1001 // Unique ID for the Process button
+
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32792
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
